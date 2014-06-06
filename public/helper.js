441addb1865b0da6c7fb72b822cc5cfb03f460ac
Rajendra <rajendra@practo.com>
2014-06-07 02:12:41 +0530

    WIP: UI changes

--- public/helper.js
+++ public/helper.js
@@ -1,3 +1,8 @@
+var calendarEvents = {
+	'eventSources' : {
+		'events' : []
+	}
+};
 $(function(){
 	$("#fc-container").fullCalendar({
 		selectable: true,
@@ -8,6 +13,32 @@ $(function(){
 			center: 'title',
 			right: 'next'
 		},
-		yearColumns: 4
+		eventColor : '#FFF',
+		eventBackgroundColor: '#333333',
+		yearColumns: 3,
+		events: calendarEvents
 	});
+	$("#sheql-querybox").submit(function(ev){
+		ev.preventDefault();
+		var view = $("#fc-container").fullCalendar('getView');
+		var viewStartDate = view.start;
+		var viewEndDate = view.end;
+		var dates = [];
+		for (i=viewStartDate.getTime();i<viewEndDate.getTime();i+=(86400000*2)){
+			dates.push(new Date(i));
+		}
+		renderEvents(dates);
+	});
+	function renderEvents(dates){
+		var events = [];
+		$.each(dates,function(key,value){
+			events.push({
+				'title' : 'Event ' + key,
+				'start' : value,
+				'allDay': true
+			});
+		});
+		calendarEvents.events = events;
+		$("#fc-container").fullCalendar('refetchEvents');
+	}
 });
