b3e053389a6e28157291748c4585f776a1bf915d
Tushar Mathur <tusharmath@gmail.com>
2014-06-07 13:30:41 +0530

    --wip--

--- public/javascripts/sheql-main.js
+++ public/javascripts/sheql-main.js
@@ -3,8 +3,75 @@ var Main = (function () {
 	function Main() {}
 	var _proto = Main.prototype;
 
-	_proto._init = function (lexer) {
+	_proto._init = function (lexer, tree) {
 		this.lexer = lexer;
+		this.tree = tree;
+	};
+
+	_proto._colonX1 = function (token) {
+		if (!token.match(/x/)) {
+			return '0';
+		}
+
+		token = token
+			.replace(/\]/, '')
+			.replace(/\:[nl]\[/, '');
+
+		if (token === 'x') {
+			return '1';
+		}
+		return token.replace('x', '');
+
+	};
+
+	_proto._colonX0 = function (token) {
+		token = token
+			.replace(/\:(n|l)/, '')
+			.replace(/\[/, '')
+			.replace(/.*x/, '')
+			.replace(/\]/, '')
+			.replace('+', '');
+		if (token === '') {
+			return '0';
+		}
+		return token;
+	};
+
+	_proto._colonType = function (token) {
+		return token.replace(/\:/, '').replace(/\[.*/, '');
+	};
+
+	_proto._colonValue = function (token) {
+
+		return {
+			type: this._colonType(token),
+			x0: parseInt(this._colonX0(token), 10),
+			x1: parseInt(this._colonX1(token), 10)
+
+		};
+	};
+
+	_proto._dotFilter = function () {};
+	_proto._colonFilter = function () {};
+
+	_proto._yearOperations = function () {
+
+	};
+
+	_proto._parseTokens = function (tokens) {
+		if (tokens.y) {
+			//Perform operations for Y
+		}
+		if (tokens.m) {
+			//Perform operations for M
+		}
+		if (tokens.w) {
+			//Perform operations for w
+		}
+		if (tokens.d) {
+			//Perform operations for d
+		}
+
 	};
 
 	_proto._buildDateRangeCollection = function (_startDate, _endDate) {
--- test/sheql-main.js
+++ test/sheql-main.js
@@ -1,9 +1,10 @@
 describe('Main', function () {
-	var main, lxr;
+	var main, lxr, tree;
 	beforeEach(function () {
 		main = new Main();
 		lxr = new Lexer();
-		main._init(lxr);
+		tree = new TreeBuilder();
+		main._init(lxr, tree);
 	});
 
 	it('expectation', function () {
@@ -25,5 +26,76 @@ describe('Main', function () {
 	describe('_getTokens()', function () {
 
 	});
+	describe('_colonType(token)', function () {
+		it('should return n', function () {
+			main._colonType(':n[2x+4]').should.equal('n');
+		});
 
+		it('should return l', function () {
+			main._colonType(':l[-2x+4]').should.equal('l');
+		});
+	});
+
+	describe('_colonX0(token)', function () {
+		it('should return 0', function () {
+			main._colonX0(':n[x]').should.equal('0');
+		});
+
+		it('should return 1', function () {
+			main._colonX0(':n[x+1]').should.equal('1');
+		});
+
+		it('should return 25', function () {
+			main._colonX0(':n[x+25]').should.equal('25');
+		});
+		it('should return 35', function () {
+			main._colonX0(':n[35]').should.equal('35');
+		});
+
+	});
+
+	describe('_colonX1(token)', function () {
+		it('should return 1', function () {
+			main._colonX1(':n[x]').should.equal('1');
+		});
+
+		it('should return 2', function () {
+			main._colonX1(':n[2x]').should.equal('2');
+		});
+
+		it('should return 25', function () {
+			main._colonX1(':n[25x]').should.equal('25');
+		});
+
+		it('should return 0', function () {
+			main._colonX1(':n[25]').should.equal('0');
+		});
+	});
+
+	describe('_colonValue(token)', function () {
+
+		it('should return for lth values', function () {
+			main._colonValue(':l[2x+1]').should.eql({
+				type: 'l',
+				x0: 1,
+				x1: 2
+			});
+		});
+
+		it('should return for lth with not x0', function () {
+			main._colonValue(':l[2x]').should.eql({
+				type: 'l',
+				x0: 0,
+				x1: 2
+			});
+		});
+
+		it('should return for lth with no x1', function () {
+			main._colonValue(':l[2]').should.eql({
+				type: 'l',
+				x0: 2,
+				x1: 0
+			});
+		});
+	});
 });
