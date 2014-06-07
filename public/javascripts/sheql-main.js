64cdc2cc06a546ed324dba85e357981636a23ef0
Tushar Mathur <tusharmath@gmail.com>
2014-06-07 13:04:37 +0530

    Adding sheql-main.js

---
+++ public/javascripts/sheql-main.js
@@ -0,0 +1,16 @@
+var Main = (function () {
+	function Main() {}
+	var _proto = Main.prototype;
+	_proto._buildDateRangeCollection = function (_startDate, _endDate) {
+		var dateCollection = [];
+		var startDate = moment(_startDate, 'YYYY-MM-DD');
+		var endDate = moment(_endDate, 'YYYY-MM-DD');
+		var iter = moment(startDate).twix(endDate).iterate("days");
+		while (iter.hasNext()) {
+			dateCollection.push(iter.next().toDate());
+		}
+		return dateCollection;
+	};
+
+	return Main;
+})();
---
+++ test/sheql-main.js
@@ -0,0 +1,23 @@
+describe('Main', function () {
+	var main;
+	beforeEach(function () {
+		main = new Main();
+	});
+
+	it('expectation', function () {
+		//EMPTY TEST
+	});
+
+	describe('buildDateRangeCollection()', function () {
+		it('should return an array of dates', function () {
+			main._buildDateRangeCollection('2013-1-10', '2014-1-10')
+				.length.should.equal(366);
+		});
+
+		it('should return an instance of date', function () {
+			main._buildDateRangeCollection('2013-1-10', '2013-1-11')[0]
+				.should.be.an.instanceof(Date);
+		});
+	});
+
+});
