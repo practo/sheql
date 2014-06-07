5efa0a5c860f5af85401ae26bc33344bdfd74379
Tushar Mathur <tusharmath@gmail.com>
2014-06-07 16:28:58 +0530

    Adding depen files

--- public/helper.js
+++ public/helper.js
@@ -1,40 +1,44 @@
 var calendarEvents = {
-	'eventSources' : {
-		'events' : []
+	'eventSources': {
+		'events': []
 	}
 };
-$(function(){
+$(function () {
 	$("#fc-container").fullCalendar({
 		selectable: true,
-		editable : false,
+		editable: false,
 		defaultView: 'year',
 		header: {
 			left: 'prev',
 			center: 'title',
 			right: 'next'
 		},
-		eventColor : '#FFF',
+		eventColor: '#FFF',
 		eventBackgroundColor: '#333333',
 		yearColumns: 3,
 		events: calendarEvents
 	});
-	$("#sheql-querybox").submit(function(ev){
+	$("#sheql-querybox").submit(function (ev) {
+
 		ev.preventDefault();
+
 		var view = $("#fc-container").fullCalendar('getView');
 		var viewStartDate = view.start;
 		var viewEndDate = view.end;
-		var dates = [];
-		for (i=viewStartDate.getTime();i<viewEndDate.getTime();i+=(86400000*2)){
-			dates.push(new Date(i));
-		}
+
+		var m = new Main();
+		m._init(new Lexer(), new TreeBuilder());
+		var dates = m.execute($('input[name="query"]').val(), viewStartDate, viewEndDate);
+
 		renderEvents(dates);
 	});
-	function renderEvents(dates){
+
+	function renderEvents(dates) {
 		var events = [];
-		$.each(dates,function(key,value){
+		$.each(dates, function (key, value) {
 			events.push({
-				'title' : 'Event ' + key,
-				'start' : value,
+				'title': 'Event ' + key,
+				'start': value,
 				'allDay': true
 			});
 		});
