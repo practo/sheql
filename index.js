3d8de62c0e2ba041550ef452b1893aeab28c65b4
tushar mathur <tushar.mathur@practo.com>
2014-07-01 08:49:17 +0000

    Adding more ignore rules for npm

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var exe = require('./src/executor.coffee')();
+var exe = require('./lib/sheql.js')();
 module.exports.getDates = function (str, startDate, endDate, startOfWeek) {
     return exe.executor(str, startDate, endDate, startOfWeek);
 };
